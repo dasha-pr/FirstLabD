0f4cecedc9476018ae30f1e9aeac137bfcd670bb
dasha-pr <svdasha2002@gmail.com>
2020-02-26 12:48:09 +0200

    FIRST lab

---
+++ FirstLabD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLabD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = 0, second = 0;
+            first = Convert.ToDouble(Console.ReadLine());
+            second = Convert.ToDouble(Console.ReadLine());
+            double result = Math.Pow((first + second), 2);
+            Console.WriteLine(result);
+        }
+    }
+}
